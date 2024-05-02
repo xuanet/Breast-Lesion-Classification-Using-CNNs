23ffcd0cf3629ab9279372637f8ac650e7386f39
xuanet <89868629+xuanet@users.noreply.github.com>
2024-05-02 05:14:13 -0400

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-print(tf.config.list_physical_devices())
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-# tf.debugging.set_log_device_placement(True)
-
-
-# from tensorflow.python.client import device_lib 
-# print(device_lib.list_local_devices())
